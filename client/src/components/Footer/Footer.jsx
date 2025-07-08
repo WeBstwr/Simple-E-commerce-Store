1e4e7f2b52aab80ce13dec6283f6622cd29cb29c
WeBstwr <ifedhawebster@gmail.com>
2025-07-08 12:14:26 +0300

    create the footer
    I have created the footer of the page.

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import './footer.css'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer className="footer-container">
+            <div className="footer-content">
+                <h2 className="footer-title">WBT Luxe</h2>
+                <p className="footer-caption">Luxury Within Reach.</p>
+                <span className="footer-est">EST: 2025</span>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
